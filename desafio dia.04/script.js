283f6ef1aa350ca41a05f79715ab577b027b015b
Estudante <aprendendoaprogramar123456@outlook.com>
2023-08-31 11:54:23 -0300

    desafio

---
+++ desafio dia.04/script.js
@@ -0,0 +1,26 @@
+let fome = false
+let dinheiro = false
+let restauranteAberto = false
+
+
+let opcaoFome = prompt("Você está com fome? (s/n)")
+if (opcaoFome == "s"){
+    fome = true
+}
+let opcaoDinheiro = prompt("Você tem dinheiro? (s/n)")
+if (opcaoDinheiro == "s"){
+    dinheiro = true
+}
+let opcaoRestaurante = prompt("O restaurante está aberto? (s/n)")
+if (opcaoRestaurante == "s"){
+    restauranteAberto = true
+}
+
+if (!fome || !dinheiro){
+    console.log("Hoje a janta será em casa")
+}else if (fome && dinheiro && !restauranteAberto){
+    console.log("Peça um delivery!")
+}else{
+    console.log("Hoje o jantar será no seu restaurante preferido!")
+}
+
